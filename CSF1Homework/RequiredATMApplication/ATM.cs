7091f2c0652d502d7fd3573b75833d6c4a9243fa
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-22 01:18:52 -0500

    Initial Commit.

---
+++ CSF1Homework/RequiredATMApplication/ATM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework.RequiredATMApplication
+{
+    class ATM
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Required: ATM Application");
+        }//end main
+    }//end class
+}//end namespace
