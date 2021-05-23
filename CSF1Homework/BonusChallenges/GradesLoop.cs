75c30e8d3b8af5309e746057c32fd9640fdd7c3e
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-23 14:03:04 -0500

    Initial commit.

---
+++ CSF1Homework/BonusChallenges/GradesLoop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework.BonusChallenges
+{
+    class GradesLoop
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Grades Loop";
+        }//end main
+    }//end class
+}//end namespace
