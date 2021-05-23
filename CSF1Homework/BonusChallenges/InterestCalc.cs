0ca873a90c77fc64452a490b95e67f3cf4b251de
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-23 15:53:38 -0500

    Initial commit

---
+++ CSF1Homework/BonusChallenges/InterestCalc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework.BonusChallenges
+{
+    class InterestCalc
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Interest Calculator";
+
+
+
+        }//end main
+    }//end class
+}//end namespae
