13ada24e5fa66ed39509edcb26a5fbb339cad6af
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-23 00:29:54 -0500

    Initial commit.

---
+++ CSF1Homework/BonusChallenges/Powers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework.BonusChallenges
+{
+    class Powers
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Powers";
+        }//end main
+    }//end class
+}//end namespace
